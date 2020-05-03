e9d9786b65903628433e326136186ebc6f1e2a53
Sander <sendongliang@gmail.com>
2020-05-03 17:33:25 +1200

    delete

--- model/article.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-var articleSchema = require('./../schema/article.js');
-const articleModel = mongoose.model('Article',articleSchema);
-module.exports =  articleModel;
--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-import {combineReducers,createStore} from 'redux';
-import baseData from './reducer/baseData/index.js';
-export default createStore(combineReducers({
-    baseData,
-}));
